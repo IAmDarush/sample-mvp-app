c7ce40dad379e3010feda38d1ad40ddb5142b5a7
DarushDev <darushfaqiri@gmail.com>
2017-12-12 17:35:02 +0800

    Create Data Manager class to access the shared preferences

---
+++ app/src/main/java/com/example/myandroidmvpsample/data/DataManager.java
@@ -0,0 +1,34 @@
+package com.example.myandroidmvpsample.data;
+
+/**
+ * Created by Darush on 12/12/2017.
+ */
+
+public class DataManager {
+
+    SharedPrefsHelper mSharedPrefsHelper;
+
+    public DataManager(SharedPrefsHelper sharedPrefsHelper) {
+        mSharedPrefsHelper = sharedPrefsHelper;
+    }
+
+    public void clear() {
+        mSharedPrefsHelper.clear();
+    }
+
+    public void saveEmailId(String email) {
+        mSharedPrefsHelper.putEmail(email);
+    }
+
+    public String getEmailId() {
+        return mSharedPrefsHelper.getEmail();
+    }
+
+    public void setLoggedIn() {
+        mSharedPrefsHelper.setLoggedInMode(true);
+    }
+
+    public boolean getLoggedInMode() {
+        return mSharedPrefsHelper.getLoggedInMode();
+    }
+}
