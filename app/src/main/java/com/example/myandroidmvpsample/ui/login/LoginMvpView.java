0f7583f14a88b1447fe1927b31e01d5a61786ae9
DarushDev <darushfaqiri@gmail.com>
2017-12-16 23:05:31 +0800

    create common utils and validate email

---
+++ app/src/main/java/com/example/myandroidmvpsample/ui/login/LoginMvpView.java
@@ -0,0 +1,12 @@
+package com.example.myandroidmvpsample.ui.login;
+
+import com.example.myandroidmvpsample.base.MvpView;
+
+/**
+ * Created by Darush on 12/16/2017.
+ */
+
+public interface LoginMvpView extends MvpView {
+    void openMainActivity();
+    void onLoginButtonClick();
+}
