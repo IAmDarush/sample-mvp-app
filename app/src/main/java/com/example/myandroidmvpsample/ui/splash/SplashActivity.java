da3381cb30c0d283ee1ceea09ace0330df833a52
DarushDev <darushfaqiri@gmail.com>
2017-12-18 00:09:15 +0800

    Attach Splash Activity to Splash Presenter. implement openLogin and openMain methods in Splash Activity.

--- app/src/main/java/com/example/myandroidmvpsample/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/myandroidmvpsample/ui/splash/SplashActivity.java
@@ -1,27 +1,46 @@
 package com.example.myandroidmvpsample.ui.splash;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.myandroidmvpsample.MvpApp;
 import com.example.myandroidmvpsample.R;
+import com.example.myandroidmvpsample.data.DataManager;
 import com.example.myandroidmvpsample.ui.base.BaseActivity;
 
 public class SplashActivity extends BaseActivity implements SplashMvpView {
 
+    SplashPresenter mSplashPresenter;
 
+    public static Intent getStartIntent(Context context) {
+        Intent intent = new Intent(context, SplashActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        DataManager dataManager = ((MvpApp) getApplication()).getDataManager();
+        mSplashPresenter = new SplashPresenter(dataManager);
+        mSplashPresenter.onAttach(this);
+        mSplashPresenter.decideNextActivity();
+
     }
 
     @Override
     public void openMainActivity() {
-
+        Intent intent = getStartIntent(this);
+        startActivity(intent);
+        finish();
     }
 
     @Override
     public void openLoginActivity() {
-
+        Intent intent = getStartIntent(this);
+        startActivity(intent);
+        finish();
     }
 }
